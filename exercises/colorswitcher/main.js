a1b940d7d4ac9cd1f2bccdee657ff3bdb1b1c48a
Krappitz <34445861+verenakrappitz@users.noreply.github.com>
2023-05-08 11:18:24 -0400

    Create main.js

---
+++ exercises/colorswitcher/main.js
@@ -0,0 +1,36 @@
+/* 
+
+Instructions: when a user clicks on one of the colored dots, the    background color of the entire page changes to match that dot. You should not need to change any HTML or CSS. 
+
+Hint: Classes for each color have already been defined in the CSS tab.
+
+*/
+
+// 1 -- Select each button
+
+let grayBtn = document.getElementById("grayButton");
+let blueBtn = document.getElementById("blueButton");
+let yellowBtn = document.getElementById("yellowButton");
+
+// 3 -- Write an event handler to change the color of the page when each circle is pressed.
+
+function switchToGray() {
+  let body = document.querySelector("body");
+  body.style.backgroundColor = "gray";
+}
+function switchToBlue() {
+  let body = document.querySelector("body");
+  body.style.backgroundColor = "blue";
+  body.style.color = "white";
+}
+
+function switchToYellow() {
+  let body = document.querySelector("body");
+  body.style.backgroundColor = "yellow";
+  body.style.color = "black";
+}
+
+// 2 -- Add an event listener to each circle
+grayBtn.addEventListener("click", switchToGray);
+blueBtn.addEventListener("click", switchToBlue);
+yellowButton.addEventListener("click", switchToYellow);
